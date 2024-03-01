3ad273cb175b7ee973f940c87a3985f949e96368
Guthox <gutho.san@gmail.com>
2024-03-01 09:55:19 -0300

    Terminado exercicio 2

--- AT2/Parte 2/exe2.c
+++ AT2/Parte 2/exe2.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 
-#define TAMANHO 9 // 3 * 3
+#define TAMANHO 3 // 3 * 3
 
-void gerarMatriz(int *matriz);
-void printarMatriz(int *matriz);
-void somarMatriz(int *matrizA, int *matrizB, int *matrizC);
-void subtrairMatriz(int *matrizA, int *matrizB, int *matrizC);
+void gerarMatriz(int matriz[TAMANHO][TAMANHO]);
+void printarMatriz(int matriz[TAMANHO][TAMANHO]);
+void somarMatriz(int matrizA[TAMANHO][TAMANHO], int matrizB[TAMANHO][TAMANHO], int matrizC[TAMANHO][TAMANHO]);
+void subtrairMatriz(int matrizA[TAMANHO][TAMANHO], int matrizB[TAMANHO][TAMANHO], int matrizC[TAMANHO][TAMANHO]);
+void multiplicarMatriz(int matrizA[TAMANHO][TAMANHO], int matrizB[TAMANHO][TAMANHO], int matrizC[TAMANHO][TAMANHO]);
+void transposta(int matriz[TAMANHO][TAMANHO]);
 int menu();
 
 int main(){
-    int matrizA[TAMANHO];
-    int matrizB[TAMANHO];
-    int matrizC[TAMANHO];
+    int matrizA[TAMANHO][TAMANHO];
+    int matrizB[TAMANHO][TAMANHO];
+    int matrizC[TAMANHO][TAMANHO];
     printf("Gerando a matriz A.\n");
     gerarMatriz(matrizA);
     printf("Gerando a matriz B.\n");
     gerarMatriz(matrizB);
 
+    printf("A=\n");
+    printarMatriz(matrizA);
+    printf("--------------\n");
+    printf("B=\n");
+    printarMatriz(matrizB);
     int op;
     do{
         op = menu();
 
         switch (op){
             case 1:
-                break; // TODO MULTIPLICAr
+                multiplicarMatriz(matrizA, matrizB, matrizC);
+                printf("A*B =\n");
+                printarMatriz(matrizC);
+                break;
             case 2:
                 subtrairMatriz(matrizA, matrizB, matrizC);
                 printf("A-B = \n");
@@ -35,11 +45,15 @@ int main(){
                 printarMatriz(matrizC);
                 break;
             case 4:
-                break; // TODO TRANSPOSTA
+                multiplicarMatriz(matrizA, matrizB, matrizC);
+                transposta(matrizC);
+                printf("Transposta de C sendo C = A*B\n");
+                printarMatriz(matrizC);
+                break;
             case 5:
                 break;
             default:
-                printf("Operacao invalida.");
+                printf("Operacao invalida.\n");
         }
 
     }while(op !=5);
@@ -58,30 +72,68 @@ int menu(){
     return op;
 }
 
-void gerarMatriz(int *matriz){
+void gerarMatriz(int matriz[TAMANHO][TAMANHO]){
+    int valor;
     for (int i = 0; i < TAMANHO; i++){
-        printf("Digite um numero: ");
-        scanf("%d", &matriz[i]);
-    }
-}
-
-void somarMatriz(int *matrizA, int *matrizB, int *matrizC){
-    for (int i = 0; i < TAMANHO; i++){
-        matrizC[i] = matrizA[i] + matrizB[i];
-    }
-}
-
-void subtrairMatriz(int *matrizA, int *matrizB, int *matrizC){
-    for (int i = 0; i < TAMANHO; i++){
-        matrizC[i] = matrizA[i] - matrizB[i];
-    }
-}
-
-void printarMatriz(int *matriz){
-    for (int i = 0; i < TAMANHO; i++){
-        printf("%d ", matriz[i]);
-        if ( (i+1) % 3 == 0){
-            printf("\n");
+        for (int j = 0; j < TAMANHO; j++){
+            printf("Digite um numero: ");
+            scanf("%d", &valor);
+            matriz[i][j] = valor;
         }
+    }
+}
+
+void somarMatriz(int matrizA[TAMANHO][TAMANHO], int matrizB[TAMANHO][TAMANHO], int matrizC[TAMANHO][TAMANHO]){
+    for (int i = 0; i < TAMANHO; i++){
+        for (int j = 0; j < TAMANHO; j++){
+            matrizC[i][j] = matrizA[i][j] + matrizB[i][j];
+        }
+    }
+}
+
+void subtrairMatriz(int matrizA[TAMANHO][TAMANHO], int matrizB[TAMANHO][TAMANHO], int matrizC[TAMANHO][TAMANHO]){
+    for (int i = 0; i < TAMANHO; i++){
+        for (int j = 0; j < TAMANHO; j++){
+            matrizC[i][j] = matrizA[i][j] - matrizB[i][j];
+        }
+    }
+}
+
+void multiplicarMatriz(int matrizA[TAMANHO][TAMANHO], int matrizB[TAMANHO][TAMANHO], int matrizC[TAMANHO][TAMANHO]){
+    int aux = 0;
+    for (int i = 0; i < TAMANHO; i++){
+        for (int j = 0; j < TAMANHO; j++){
+            matrizC[i][j] = 0;
+            for (int k = 0; k < TAMANHO; k++){
+                aux = aux + ( matrizA[i][k] * matrizB[k][j] );
+            }
+            matrizC[i][j] = aux;
+            aux = 0;
+        }
+    }
+}
+
+void transposta(int matriz[TAMANHO][TAMANHO]){
+    int transposta[TAMANHO][TAMANHO];
+    for (int i = 0; i < TAMANHO; i++){
+        for (int j = 0; j < TAMANHO; j++){
+            transposta[i][j] = matriz[j][i];
+        }
+    }
+    
+    for (int i = 0; i < TAMANHO; i++){
+        for (int j = 0; j < TAMANHO; j++){
+            matriz[i][j] = transposta[i][j];
+        }
+    }
+
+}
+
+void printarMatriz(int matriz[TAMANHO][TAMANHO]){
+    for (int i = 0; i < TAMANHO; i++){
+        for (int j = 0; j < TAMANHO; j++){
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
     }
 }
